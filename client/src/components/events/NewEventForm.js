fc475ec32dc102a7b6293dc8e5b95835dd4d7bb5
Xinyi Wu <xxinyiwho@gmail.com>
2020-03-01 15:27:29 +0100

    small changes

--- client/src/components/events/NewEventForm.js
+++ client/src/components/events/NewEventForm.js
@@ -3,6 +3,8 @@ import React from 'react';
 
 // NEW EVENT FORM (using hooks made it easier than normal form like I did in edit event form)
 const NewEventForm = ({ onNewEvent = f => f }) => {
+
+  // DECLARE MULTIPLE STATE VARIABLE
   let title, description, start_date, end_date
 
   // CALL FUNCTION WHEN SUBMIT
